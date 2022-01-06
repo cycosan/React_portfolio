f9ca9afc866ffd579123981de4dc9e6790399704
unknown <sanjay@dhuni.io>
2022-01-06 17:27:27 +0545

    initial

--- src/components/About.js
+++ src/components/About.js
@@ -21,7 +21,7 @@ export default function About(){
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                 Sanjay Maharjan.
                 <br className="hidden lg:inline-block" />
-                I'm <span ref={textRef}></span>
+                I'm <span ref={textRef} className="text-red-500" ></span>
               </h1>
               <p className="mb-8 leading-relaxed">
                Currently working as a Mid-Level Python Developer.I am very fond of learning new technologies and experience.
