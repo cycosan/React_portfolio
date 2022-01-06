09d10176ce003ed6d54b6664703a344d00d9efd5
unknown <sanjay@dhuni.io>
2022-01-06 14:25:35 +0545

    deploy

---
+++ src/data.js
@@ -0,0 +1,56 @@
+import Image from "./image.jpg"
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: Image,
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: Image,
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: Image,
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: Image,
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+  export const skills = [
+   "Python","React","Heroku","ASV.Net","Java","Html/CSS"
+  ];
+
+
+  
+  export const testimonials = [
+    {
+      "quote":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "image":Image,
+      "name":"Bishal Tamang",
+      "company":"Kantipur Engineering College"
+    },
+    {
+      "quote":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "image":Image,
+      "name":"Bishal Tamang",
+      "company":"Kantipur Engineering College"
+    }
+   ];
